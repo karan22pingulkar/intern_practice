a151ff86268dded24b18d52f85e6c4fdc06b1683
karan <karan22pingulkar@gmail.com>
2026-08-11 15:54:31 +0530

    added changes

--- cfd_monitor.py
+++ cfd_monitor.py
@@ -1,15 +1,23 @@
 class Room:
-    def __init__(self, name, temp):
+    def __init__(self, name, temp, humidity):
         self.name = name
         self.temp = temp
+        self.humidity = humidity
 
     def update_temp(self, new_temp):
         self.temp = new_temp
 
+    def __str__(self):
+        return f"Room: {self.name}, Temp: {self.temp}°C, Humidity: {self.humidity}%"
 
-# lab = Room("server_room", 24)
-# print(lab.temp)
+    def check_humidity(self):
+        if self.humidity > 90:
+            message = "Warning: Environment is too humid!"
+        else:
+            message = "Humidity levels are normal."
 
-# lab.update_temp(24.5)
+        return message
 
-# print(lab.temp)
+
+server = Room("server_room", 24, 90)
+# print(server)
